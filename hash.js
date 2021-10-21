5fc229b469f3123516c066e52581be9bca0e0961
rrgamata <raph.amata69@gmail.com>
2021-10-22 00:22:57 +0800

    Updated blog link by setting title as the link

--- hash.js
+++ hash.js
@@ -32,7 +32,7 @@ console.table(gql(GET_USER_ARTICLES,{page:0}))
 gql(GET_USER_ARTICLES, { page: 0 })
     .then(result => {
         const articles = result.data.user.publication.posts;
-        let aLimit = articles.slice(0,3);
+        let aLimit = articles.slice(0,6);
         let container = document.createElement('div');
         container.className = "blog-container backdrop-grid";
         aLimit.forEach(article => {
@@ -42,21 +42,23 @@ gql(GET_USER_ARTICLES, { page: 0 })
             image.src = article.coverImage;
             image.className = "blog-img";
 
+            /*
             let title = document.createElement('h2');
             title.innerText = article.title;
             title.className = "blog-title";
+            */
 
            /* let brief = document.createElement('p');
             brief.innerText = article.brief;
             brief.className = "blog-brief";
             */
             let link = document.createElement('a');
-            link.innerText = 'Read more...';
+            link.innerText = article.title;
             link.href = `https://cifer628.hashnode.dev/${article.slug}`;
             link.className = "blog-link";
 
             eBlogContainer.appendChild(image);
-            eBlogContainer.appendChild(title);
+            /*eBlogContainer.appendChild(title);*/
            // eBlogContainer.appendChild(brief);
             eBlogContainer.appendChild(link);
             container.appendChild(eBlogContainer);
